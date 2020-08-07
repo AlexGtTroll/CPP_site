0b5117ba32eec12421e63c7ebd8763f3e1a82839
AlexGtTroll <www.synicate@mai.ru>
2020-08-07 16:41:08 +0700

    updated files

--- pages/autosave.php
+++ pages/autosave.php
@@ -29,42 +29,79 @@
 
 <div style = "position: relative">
 <form method = "POST" action = "">
-	<input name = "obj" type = "text" placeholder = "Объект"/>
-		<input name = "nameus" type = "text" placeholder = "Ваше имя"/>
-	<input name = "comment" type = "text" placeholder = "Комментарий.." />
-	<input type = "submit" value = "Отправить"/>
+	<input name = "obj" type = "text" placeholder = "Объект" size = "22"/>
+		<p><input name = "name" type = "text" placeholder = "Ваше имя" size = "26"/></p>
+	<input name = "comment" type = "text" placeholder = "Комментарий.." size = "30"/>
+	<p><input type = "submit" value = "Отправить"/></p>
 	</div>
 
 <?php
+//id obj name comment note
 error_reporting(E_ALL);
+
 ////Переменные с формы////
-//$obj = $_POST['obj'];
-//$comment = $_POST['comment'];
-//$nameus = $_POST['nameus'];
-	$obj = "Kotel №1";
-	$comment = "Everything is fine";
-	$nameus = "Jotaro";
-//---------Данные для подключения-------------
+$obj = $_POST['obj'];
+/**/$obj = mb_convert_encoding($_POST['obj'], "Windows-1251", "UTF-8");//
+$name = $_POST['name'];
+/**/$name = mb_convert_encoding($_POST['name'], "Windows-1251", "UTF-8");//
+$comment = $_POST['comment'];
+/**/$comment = mb_convert_encoding($_POST['comment'], "Windows-1251", "UTF-8");//
+
+//---------Данные для подключения-------------//
 $server = 'NEKRONAMICON\SANYA';
-$namecon = 'sa';
+$namesa = 'sa';
 $pass = '123';
 $database = 'test';
 $db_table = 'ext';
 /////////////////////////////////////////////
-$link = mssql_connect($server, $namecon, $pass);
+$link = mssql_connect($server, $namesa, $pass);
 if (!$link){
 	die ('Error: '. mssql_get_last_message());
 }
 
 mssql_select_db($database,$link);
 /////////////////////////////////////////////
-$result = mssql_query("INSERT INTO ext VALUES ('$obj', '$nameus', '$comment')");
+if (empty($obj) || empty($name) || empty($comment)){
+	echo "Заполните все поля";
+}else{
+$result = mssql_query("INSERT INTO ext (obj, name, comment) VALUES ('$obj', '$name', '$comment')");
 if ($result == true){
 	echo "Информация занесена в базу данных";
 }else{
 	echo "Информация не занесена в базу данных";
 }
+}
+unset($obj); unset($name); unset($comment);
+mssql_close($link);
 ?>
+
+<div id = "current_date_time_block"></div>
+
+<script type = "text/javascript">
+function zero_first_format(value){
+	if (value<10){
+		value = '0' + value;
+	}
+	return value;
+}
+
+function date_time(){
+	var current_datetime = new Date();
+	var day = zero_first_format(current_datetime.getDate());
+	var month = zero_first_format(current_datetime.getMonth()+1);
+	var year = current_datetime.getFullYear();
+	var hours = zero_first_format(current_datetime.getHours());
+	var minutes = zero_first_format(current_datetime.getMinutes());
+	var seconds = zero_first_format(current_datetime.getSeconds());
+
+	return day+"."+month+"."+year+" "+hours+":"+minutes+":"+seconds;
+}
+
+setInterval(function(){
+	document.getElementById('current_date_time_block').innetHTML = date_time();
+},1000);
+</script>
+
 <!--<div style = "position: relative">
 <form method = "POST" action = "">
 	<p><b>Отчитываться сразу снизу прямо здесь и сейчас (для кочегаров)</b></p>
